f90edcb7273ab7e721d03f4ab1402eda3fbd81e3
Norris Jay Perez <jay@apptivate.co>
2014-09-18 16:52:46 +0800

    adds template

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  match '/template' => 'home#template', via: :get
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
