33f2ddc4c242c8f2480209cd379bf0c38c16c5a3
Norris Jay Perez <jay@apptivate.co>
2014-09-20 02:26:19 +0800

    adds content and fixes assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   def index
   end
 
+  def download_pdf
+    send_file(
+      "#{Rails.root}/public/2014.pdf",
+      filename: "2014.pdf",
+      type: "application/pdf"
+    )
+  end
+
   def template
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  get "home/download_pdf"
   match '/template' => 'home#template', via: :get
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
